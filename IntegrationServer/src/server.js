c5c25adffd9502e53b139aac6a42421b933bc4e1
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-04-29 23:13:44 +0000

    got some progress on setting up swagger, but it's not showing the docs yet..

--- IntegrationServer/src/server.js
+++ IntegrationServer/src/server.js
@@ -1,20 +1,43 @@
-require('dotenv').config(); //required for us to use process.env
-var express = require('express'); //node.js express
-var bodyParser = require('body-parser'); //node.js filter for POSTs
-var app = express(); //express instance.
+require('dotenv').config(); 
+var express = require('express'); 
+var bodyParser = require('body-parser'); 
+var app = express();
+const cors = require('cors');
+
 const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
 
 const routes = require('./routes'); //Import all of the exported router objects from the routes folder into this file.
 //Telling the app to "listen at" with routes passed in will enable all the defined endpoints.
 const {setOAuthCredentials} = require('./startup-helper.js');
 const {loadConfigVariables} = require('./startup-helper.js');
-const { specs } = require('./util/swagger.js')
 
 //require file to make it's code run upon startup.
 require('./OAuth/token-store-periodic.js'); //temporary access token refresher - schedules itself to run periodically when loaded, to keep the access token from expiring
 
-app.use(bodyParser.json()) //Have all requests filtered through bodyParser so that the body of all the POST requests sent to the API to be read and used. 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Contacts Integration API",
+      version: "2.0.0",
+      description: "A contacts integration between Monday.com and Google Contacts"
+    },
+    servers: [
+      {
+        url: "https://sbuseniorprojectv2.peterwelter.repl.co/"
+      }
+    ]
+  },
+  apis: ['./routes/*.js']
+};
 
+const specs = swaggerJsDoc(options)
+
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(specs));
+
+app.use(bodyParser.json()) //Have all requests filtered through bodyParser so that the body of all the POST requests sent to the API to be read and used. 
+app.use(cors());
 //Print the method, path, and ip of all requests. This will act as middleware that all requests are filtered through.
 app.use(function(req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
@@ -22,7 +45,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
 //run startup functions
 setOAuthCredentials(); //IF token.json exists (aka OAuth Credentials), load them.
@@ -41,9 +63,8 @@ if(run == "Dev") { //custom tunnel - currently set for loca.lt (localTunnel; not
   });
 
 } else { //replit
-  //Tell the app to listen at port, and then create a tunnel.
-  app.listen(port, () => { //Request replit to use a specific port for node.js to run on.
-    (`Listening on port: ${port}`); //replit has its own stuff for node.js setups which run automatically when started.
+  app.listen(port, () => { 
+    console.log(`Listening on port: ${port}`); 
   });
 }
 
