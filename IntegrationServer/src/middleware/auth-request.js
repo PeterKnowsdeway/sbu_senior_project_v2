ca074e4dd7529523d145a66f374474334d4e2f74
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-05-01 18:09:17 +0000

    Formated auth-request, added jsdoc comments and added error logging. LVSP-124

--- IntegrationServer/src/middleware/auth-request.js
+++ IntegrationServer/src/middleware/auth-request.js
@@ -1,18 +1,31 @@
 const jswtoken = require('jsonwebtoken'); //get the json webtoken library.
+const { logger } = require("../middleware/logger.js")
+const { v4: uuidv4 } = require('uuid');
+const ID = uuidv4();
 
-//This function authenticates that the request that got sent from our Monday.com App. It uses the signing secrete and JWT to try and decript 
-//some expected information in the request. If it succeeds, it stores some data to the session, and allows the request through, if it fails, the request
-//is stopped.
+/**
+  Authenticates that the request sent from the Monday.com App is valid and authorized. 
+  It uses the JSON Web Token (JWT) library to decrypt the expected information in the request using the signing secret. 
+  If the authentication succeeds, it stores some data to the session and allows the request to continue. 
+  If it fails, the request is stopped. 
+  @async
+  @function
+  @param {Object} req - The HTTP request object.
+  @param {Object} res - The HTTP response object.
+  @param {function} next - The next function to be executed.
+  @throws {Error} - If there is an error while authenticating the request with Monday.com or if the request is not authenticated.
+*/
+
 async function authRequestMiddleware(req, res, next) {
   console.log("I made it to authRequestMiddleWare.js");
-	try{
-		let authorization = req.headers.authorization;  //get the authentication info from the request. 
+	try {
+		let authorization = req.headers.authorization;  // Get the authentication info from the request. 
 		if (!authorization && req.query) {
 			authorization = req.query.token;
 		}
 		
-		//at this point, we actually try and verify the request. 
-		//If the verifiy function fails, then we know that the request wasn't sent from our Monday app.
+		// At this point, we actually try and verify the request. 
+		// If the verifiy function fails, then we know that the request wasn't sent from our Monday app.
 		const {accountId, userId, backToUrl, shortLivedToken } = jswtoken.verify(
 			authorization,
 			process.env.MONDAY_SIGNING_SECRET
@@ -22,7 +35,15 @@ async function authRequestMiddleware(req, res, next) {
 		next();
 	}
 	catch (err) {
-    console.error(err);
+    logger.error({
+      pid: process.pid,
+      requestID: ID,
+      message: `Error: An error occcured while authenticating with Monday.com: ${err}`,
+      function: 'authRequestMiddleware',
+      params: { accountId, userId, backToUrl, shortLivedToken },
+      stacktrace: err.stack
+    })
+    throw error
     res.status(500).json({ error: 'not authenticated' });
   }
 }
