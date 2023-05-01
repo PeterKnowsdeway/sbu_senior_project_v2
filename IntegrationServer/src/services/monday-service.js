7f6b21ecc4f5a88131c018421165a347174cb9f7
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-05-01 20:05:22 +0000

    Added logging to monday services

--- IntegrationServer/src/services/monday-service.js
+++ IntegrationServer/src/services/monday-service.js
@@ -1,4 +1,7 @@
 const initMondayClient = require('monday-sdk-js');
+const { logger } = require('../../middleware/logger.js')
+const { v4: uuidv4 } = require('uuid');
+const ID = uuidv4()
 
 /**
  * It takes a token and a boardId as parameters, and returns the items on the board.
@@ -6,32 +9,40 @@ const initMondayClient = require('monday-sdk-js');
  * @param boardId - The ID of the board you want to get items from.
  * @returns An array containing the Board items queried 
  */
-console.log("I made it to monday-service.js");
+
 const getBoardItems = async (token, boardId) => {
   try {
     const mondayClient = initMondayClient();
     mondayClient.setToken(token);
 
     const query = `query ($boardId: [Int]){
-					boards(limit:1 ids:$boardId) {
-						name
-						items {
-							name 
-							updated_at
-							column_values {
-								id
-								title
-								text
-							}
-						}
-					}
-				}`;
-    const variables = {boardId};
+      boards(limit:1 ids:$boardId) {
+        name
+        items {
+          name 
+          updated_at
+          column_values {
+            id
+            title
+            text
+          }
+        }
+      }
+    }`;
+    const variables = { boardId };
 
     const response = await mondayClient.api(query, { variables });
     return response.data.boards[0].items;
   } catch (err) {
-    console.error(err);
+    logger.error({
+      pid: process.pid,
+      requestID: ID,
+      message: `Error: An error occured during query: ${err}`,
+      function: 'getBoardItems',
+      params: {(token, boardId },
+      stacktrace: err.stack
+    })
+    throw err
   }
 };
 
