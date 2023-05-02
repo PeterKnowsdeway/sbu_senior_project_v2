00aadf52d1ec4cb8f27e83bb2d87bf109340ada5
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-05-02 14:25:13 +0000

    Updated logging and comments for the google-services

--- IntegrationServer/src/services/google-services/update-service.js
+++ IntegrationServer/src/services/google-services/update-service.js
@@ -1,21 +1,26 @@
 const { google } = require('googleapis');
 const OAuth2Client = require('../../OAuth/google-auth.js').OAuthClient
 google.options({auth: OAuth2Client});
-
 const service = google.people( {version: 'v1', auth: OAuth2Client});
-
 const contactMappingService = require('../database-services/contact-mapping-service');
-
+const { logger } = require("../../middleware/logger.js")
+const { v4: uuidv4 } = require('uuid');
+const ID = uuidv4();
 
 /**
- * When called, will push information for the titles located in the env are for the specified item 
- * @param itemID - specifies the item that has been changed
- * @param itemMap - contains the information to update object - req payload from monday.com
- * @param [callback] - what function to call in case of failure
- *        // TODO: CHECK callback param: is this something to replace with a const variable due to possible security concerns?
- */
-async function updateContactService (name, nameArr, arrEmails, arrPhoneNumbers, arrNotes, itemID) { // updates existing database.
-  console.log('I made it to updateExistingService')
+
+  Updates an existing contact in Google People API and updates the internal contact mapping in the database
+  @async
+  @function updateContactService
+  @param {string} name - The display name of the contact
+  @param {Array.<string>} nameArr - The array containing the first, middle and last names of the contact respectively
+  @param {Array.<Object>} arrEmails - The array containing the email addresses of the contact
+  @param {Array.<Object>} arrPhoneNumbers - The array containing the phone numbers of the contact
+  @param {Array.<Object>} arrNotes - The array containing the biographies of the contact
+  @param {number} itemID - The ID of the contact to be updated
+  @returns {null}
+*/
+async function updateContactService (name, nameArr, arrEmails, arrPhoneNumbers, arrNotes, itemID) { 
 
   let itemMapping = await contactMappingService.getContactMapping(itemID)
 
@@ -23,61 +28,58 @@ async function updateContactService (name, nameArr, arrEmails, arrPhoneNumbers, 
     resourceName: itemMapping.dataValues.resourceName,
     personFields: 'metadata'
   }, async (err, res) => {
-    if (err) return console.error('The API returned an error at update1: ' + err)
-    else {
-      let updatedMapping = await contactMappingService.getContactMapping(itemID)
-      console.log("outer service")
-
-      //THIS IS BROKEN ATM - PUSH WIPES ALL INFORMATION INSTEAD; SEE sync-contacts.js
-      //FOR THE UPDATE CASE IN FUNCTION updateExistingContact FOR COMPARISON
-      await service.people.updateContact({
-        resourceName: updatedMapping.dataValues.resourceName,
-        sources: 'READ_SOURCE_TYPE_CONTACT',
-        updatePersonFields: 'biographies,emailAddresses,names,phoneNumbers',
-        requestBody: {
-          etag: updatedMapping.dataValues.etag,
-          names: [
-            {
-              displayName: name,
-              givenName: nameArr[0],
-              middleName: nameArr[1],
-              familyName: nameArr[2],
-            }
-          ],
-          emailAddresses: arrEmails,
-          phoneNumbers: arrPhoneNumbers,
-          biographies: arrNotes
-        }
-      }, async (err, res) => {
-        if (err) console.error('The API returned an error at update2: ' + err)
-        else {
-          console.log("inner update service")
-          await contactMappingService.updateContactMapping(itemID, { resourceName: res.data.resourceName, etag: res.data.etag })
-        }
-      })//end inner service
-    }
-  }) //end outer service
-  return null
+    if (err) {
+      logger.error({
+        pid: process.pid,
+        requestID: ID,
+        message: `Error: An error occcured while updating contact: ${err}`,
+        function: 'updateContactService',
+        params: { name, nameArr, arrEmails, arrPhoneNumbers, arrNotes, itemID },
+        stacktrace: err.stack
+      })
+      throw error
+      return res.status(404).json({ error: 'The People API returned an error' })
+    } else {
+        let updatedMapping = await contactMappingService.getContactMapping(itemID)
+        await service.people.updateContact({
+          resourceName: updatedMapping.dataValues.resourceName,
+          sources: 'READ_SOURCE_TYPE_CONTACT',
+          updatePersonFields: 'biographies,emailAddresses,names,phoneNumbers',
+          requestBody: {
+            etag: updatedMapping.dataValues.etag,
+            names: [
+              {
+                displayName: name,
+                givenName: nameArr[0],
+                middleName: nameArr[1],
+                familyName: nameArr[2],
+              }
+            ],
+            emailAddresses: arrEmails,
+            phoneNumbers: arrPhoneNumbers,
+            biographies: arrNotes
+          }
+        }, async (err, res) => {
+          if (err) {
+             logger.error({
+              pid: process.pid,
+              requestID: ID,
+              message: `Error: An error occcured while updating contact: ${err}`,
+              function: 'updateContactService',
+              params: { name, nameArr, arrEmails, arrPhoneNumbers, arrNotes, itemID },
+              stacktrace: err.stack
+            })
+            throw error
+            return res.status(500).json({ error: 'The People API returned an error' })
+          } else {
+            await contactMappingService.updateContactMapping(itemID, { resourceName: res.data.resourceName, etag: res.data.etag })
+          }
+        })
+      }
+  }) 
+  return res.status(201).json({ info: 'Success' })
 }
 
-async function nameSplit(name) {
-    let nameArr = await name.split(" ");
-
-  //If there is no middle, the last name needs to be assigned to nameArr[2] for the api call
-  switch (nameArr.length == 2) {
-    case 1 :
-        nameArr[1]= "";
-        nameArr[2]= "";
-        break;
-    case 2 :
-        nameArr[2] = nameArr[1];
-        nameArr[1] = "";
-        break;
-    case 3 :
-      break;
-  }
-  return nameArr;
-}
 
 module.exports = {
   updateContactService
