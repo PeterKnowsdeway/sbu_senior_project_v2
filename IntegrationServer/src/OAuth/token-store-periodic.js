0aa2c00222925e58d1e487a4fc53fb7563f75881
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-05-03 18:06:46 +0000

    Updated some of the styling for token-store

--- IntegrationServer/src/OAuth/token-store-periodic.js
+++ IntegrationServer/src/OAuth/token-store-periodic.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
-const { google } = require('googleapis');
+const fs = require('fs')
+const { google } = require('googleapis')
 
 const TOKEN_PATH = './token.json'
 
 const OAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.BACK_TO_URL)
+  process.env.BACK_TO_URL
+)
 
 // Declares the necessary scopes from Google
 const SCOPES = ['https://www.googleapis.com/auth/contacts']
 
 // load the existing token from the token.json file
-const token = fs.readFileSync(TOKEN_PATH);
+const token = fs.readFileSync(TOKEN_PATH)
 
 // set the credentials of the OAuth2 client to the existing token
-OAuth2Client.setCredentials(JSON.parse(token));
+OAuth2Client.setCredentials(JSON.parse(token))
 
 // get a new access token and refresh token
 async function getNewToken(req, res) {
@@ -24,20 +25,15 @@ async function getNewToken(req, res) {
     prompt: 'consent',
     scope: SCOPES
   });
-  console.log('Authorize this app by visiting this url:', url);
-  console.log("I made it here")
-  const code = req.query.code; 
-  console.log("I made it here")
-  const { tokens } = await OAuth2Client.getToken(code);
-  console.log(tokens);
-  OAuth2Client.setCredentials(tokens);
-  fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
-  console.log('New access token and refresh token have been obtained and stored in token.json');
+  console.log('Authorize this app by visiting this url:', url)
+  const code = req.query.code 
+  const { tokens } = await OAuth2Client.getToken(code)
+  console.log(tokens)
+  OAuth2Client.setCredentials(tokens)
+  fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens))
+  console.log('New access token and refresh token have been obtained and stored in token.json')
 }
 
 module.exports = {
 	getNewToken
-};
-
-
-//look in ouath client for credentials, if they exist, override whtat is token.json
+}
