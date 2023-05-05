ac9c75b1af32d85fc31216b2e0378dff9efd5f5b
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-05-05 15:40:55 +0000

    Updated documentation and formatting for all the routes.

--- IntegrationServer/src/routes/contacts-integration.js
+++ IntegrationServer/src/routes/contacts-integration.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const rateLimiterUsingThirdParty = require('../middleware/rateLimiter.js');
+/**
+  * Express router for the contacts integration routes.
+  * @module contacts-integration.js
+*/
+const express = require('express')
+const router = express.Router()
+const rateLimiterUsingThirdParty = require('../middleware/rateLimiter.js')
+const makeNewContact = require('../featureControl/make-contact.js').makeNewContact
+const updateContact = require('../featureControl/update-contact.js').updateContactInfo
+const authenticationMiddleware = require('../middleware/auth-request').authRequestMiddleware
+const { fetchContacts } = require('../featureControl/sync-contacts.js')
+const Mutex = require('async-mutex').Mutex
 
-const makeNewContact = require('../featureControl/make-contact.js').makeNewContact;   
-const updateContact = require('../featureControl/update-contact.js').updateContactInfo;
-const authenticationMiddleware = require('../middleware/auth-request').authRequestMiddleware;
-const {fetchContacts} = require('../featureControl/sync-contacts.js');
-const Mutex = require('async-mutex').Mutex;
+const mutex = new Mutex()
 
-const mutex = new Mutex();
-
-router.use(rateLimiterUsingThirdParty);
+router.use(rateLimiterUsingThirdParty)
 
 /**
  * @swagger
  * /create:
  *   post:
  *     summary: Create a new contact in Google Contacts from a Monday.com item.
- *     tags: 
+ *     tags:
  *       - Contacts Integration
  *     security:
  *       - bearerAuth: []
@@ -51,34 +54,124 @@ router.use(rateLimiterUsingThirdParty);
  *     responses:
  *       200:
  *         description: OK
+ *       400:
+ *         description: Bad request. The request body is missing or invalid.
+ *       401:
+ *         description: Unauthorized. The user is not authenticated or authorized to access this resource.
  *       500:
  *         description: Internal Server Error
  */
 
 router.post('/create', authenticationMiddleware, async (req, res) => {
   await mutex.runExclusive(async () => {
-    await makeNewContact(req, res);
-  });
-});
+    await makeNewContact(req, res)
+  })
+})
+
+/**
+ * @swagger
+ * /update:
+ *   post:
+ *     summary: Update a contact in Google Contacts from a Monday.com board item.
+ *     tags:
+ *       - Contacts Integration
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       description: The Monday.com item information.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               payload:
+ *                 type: object
+ *                 properties:
+ *                   inboundFieldValues:
+ *                     type: object
+ *                     properties:
+ *                       itemMapping:
+ *                         type: object
+ *                         properties:
+ *                           name:
+ *                             type: string
+ *                           email:
+ *                             type: string
+ *                           phone:
+ *                             type: string
+ *                           notes:
+ *                             type: string
+ *                       itemId:
+ *                         type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request. The request body is missing or invalid.
+ *       401:
+ *         description: Unauthorized. The user is not authenticated or authorized to access this resource.
+ *       500:
+ *         description: Internal Server Error
+ */
 
 router.post('/update', authenticationMiddleware, async (req, res) => {
   await mutex.runExclusive(async () => {
-    await updateContact(req, res);
-  });
-});
+    await updateContact(req, res)
+  })
+})
+
+/**
+ * @swagger
+ * /sync:
+ *   post:
+ *     summary: Sync all contacts in Google Contacts from a Monday.com board.
+ *     tags:
+ *       - Contacts Integration
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       description: The Monday.com item information.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               payload:
+ *                 type: object
+ *                 properties:
+ *                   inboundFieldValues:
+ *                     type: object
+ *                     properties:
+ *                       itemMapping:
+ *                         type: object
+ *                         properties:
+ *                           name:
+ *                             type: string
+ *                           email:
+ *                             type: string
+ *                           phone:
+ *                             type: string
+ *                           notes:
+ *                             type: string
+ *                       itemId:
+ *                         type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request. The request body is missing or invalid.
+ *       401:
+ *         description: Unauthorized. The user is not authenticated or authorized to access this resource.
+ *       500:
+ *         description: Internal Server Error
+ */
 
 router.post('/sync', authenticationMiddleware, async (req, res) => {
   await mutex.runExclusive(async () => {
-    await fetchContacts(req, res);
-  });
-});
+    await fetchContacts(req, res)
+  })
+})
 
-router.post('/print', authenticationMiddleware, async (req, res) => {
-  await mutex.runExclusive(async () => {
-    console.log(req.body);
-    console.log('printRequest', JSON.stringify(req.body));
-    return res.status(200).send({});
-  });
-});
-
-module.exports = router;
+module.exports = router
